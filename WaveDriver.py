4f0528ad14b3866dd80b90f84cea437b56042e69
rladuddbs <duddbs442@naver.com>
2023-10-22 21:37:22 +0900

    boat 애니메이션 추가

--- WaveDriver.py
+++ WaveDriver.py
@@ -2,20 +2,24 @@ from pico2d import *
 
 class Boat:
     def __init__(self):
-        self.image = load_image('boat.png')
+        self.image = load_image('boat_animation.png')
+        self.frame = 0
 
     def draw(self):
-        self.image.clip_draw(0, 0, 500, 500, 600, 200, 150, 150)
+        self.image.clip_draw(400 * self.frame, 0, 400, 400, 600, 400, 200, 200)
 
     def update(self):
-        pass
+        self.frame = (self.frame + 1) % 3
 
 open_canvas(1200, 800)
 boat = Boat()
 
 while True:
     clear_canvas()
+    boat.update()
     boat.draw()
+    delay(0.1)
     update_canvas()
+
 
 close_canvas()
