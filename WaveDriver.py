099880d6d40a22ce7c25034668018438d2cc26e7
rladuddbs <duddbs442@naver.com>
2023-10-30 11:27:52 +0900

    반복문 종료 조건 추가

--- WaveDriver.py
+++ WaveDriver.py
@@ -7,29 +7,32 @@ from boat import Boat
 from stone import Stone
 from sea import Sea
 
+
 root = Tk()
 
 monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
 
 open_canvas(monitor_width, monitor_height)
-boat = Boat()
-stone = Stone()
+
 
 def handle_events():
+    global mx, my, running
+
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, monitor_height - 1 - event.y
+        if event.type == SDL_MOUSEMOTION:
+            mx, my = event.x, monitor_height - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             close_canvas()
+            running = False
+
 
 def create_world():
     global boat
     global stone
     global sea
+    global cursor
 
     sea = Sea()
     GameWorld.add_object(sea)
@@ -40,16 +43,21 @@ def create_world():
     stone = Stone()
     GameWorld.add_object(stone)
 
+    cursor = load_image('paddle.png')
 
 
 def render_world():
     clear_canvas()
     GameWorld.render()
+    cursor.draw(mx, my)
     update_canvas()
 
-create_world()
 
-while True:
+hide_cursor()
+create_world()
+running = True
+
+while running:
     handle_events()
     GameWorld.update()
     render_world()
