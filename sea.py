c130550c8e41cbe10c78f4c7e4b7da17c6888ba3
rladuddbs <duddbs442@naver.com>
2023-11-13 00:56:48 +0900

    바다 움직임 확인

--- sea.py
+++ sea.py
@@ -5,12 +5,27 @@ root = Tk()
 
 monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
+
+
+# Boy Run Speed
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+MOVE_SPEED_KMPH = 20.0  # Km / Hour
+MOVE_SPEED_MPM = (MOVE_SPEED_KMPH * 1000.0 / 60.0)
+MOVE_SPEED_MPS = (MOVE_SPEED_MPM / 60.0)
+MOVE_SPEED_PPS = (MOVE_SPEED_MPS * PIXEL_PER_METER)
+
+# Boy Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class Sea:
     def __init__(self):
         self.image = load_image('sea.png')
+        self.img1_y = 0
 
     def draw(self):
-        self.image.draw(monitor_width / 2, monitor_height / 2)
-
+        self.image.clip_draw(0, 0, 1980, 1080, monitor_width / 2, monitor_height / 2 + self.img1_y)
+        self.img1_y -= 0.1
     def update(self):
         pass
