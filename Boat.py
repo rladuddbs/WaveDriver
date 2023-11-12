6b6d0eac7c34c7a4d75ce6e702d74b682bb4304b
rladuddbs <duddbs442@naver.com>
2023-11-13 00:11:21 +0900

    boat 좌표를 변수로 변경

--- Boat.py
+++ Boat.py
@@ -15,18 +15,26 @@ class Boat:
         self.mx = 0
         self.my = 0
         self.click = False
+        self.boat_x, self.boat_y = monitor_width / 2, 200
 
     def draw(self):
         if self.mx >= monitor_width / 2:
-            self.image.clip_draw(400 * self.frame, 0, 400, 400, monitor_width / 2, 200, 200, 200)
+            self.image.clip_draw(400 * self.frame, 0, 400, 400, self.boat_x, self.boat_y, 200, 200)
         else:
-            self.image.clip_composite_draw(400 * self.frame, 0, 400, 400, 0, 'h', monitor_width / 2, 200, 200, 200)
-
+            self.image.clip_composite_draw(400 * self.frame, 0, 400, 400, 0, 'h', self.boat_x, self.boat_y, 200, 200)
 
 
     def GetClickImpo(self, click, mx, my):
         self.click = click
         self.mx, self.my = mx, my
+
+    def GetMousePos(self, mx, my):
+        self.mx, self.my = mx, my
+
+    def move(self):
+
+        pass
+
 
     def update(self):
         if not self.click:
@@ -40,3 +48,5 @@ class Boat:
                 self.frame = 2
 
         print(self.click)
+
+
