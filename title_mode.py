07ea854d6af32cfb70d965aca884b15f6dae6000
rladuddbs <duddbs442@naver.com>
2023-12-02 01:46:32 +0900

    타이틀 보트 애니매이션 추가

--- title_mode.py
+++ title_mode.py
@@ -4,6 +4,7 @@ from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
 import game_framework
 import game_world
 import play_mode
+from boat import Boat
 from sea import Sea
 
 from tkinter import *
@@ -14,17 +15,25 @@ monitor_height = root.winfo_screenheight()
 monitor_width = root.winfo_screenwidth()
 
 def init():
-    global sea, image
+    global sea, image, boat, boat_x, boat_y, spd
     sea = Sea()
     game_world.add_object(sea)
+
+    boat = Boat()
+    game_world.add_object(boat)
+
+    boat_x, boat_y = monitor_width / 2, -100
+    spd = 0.5
     pass
 
 
 def finish():
     pass
 
+animation_start = False
 
 def handle_events():
+    global animation_start
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -32,18 +41,28 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            game_framework.change_mode(play_mode)
+            animation_start = True
     pass
 
 
 def update():
+    global boat_y, animation_start, spd
+    if animation_start:
+        boat_y += spd
+        spd -= 0.0004
+    if boat_y >= 200:
+        game_framework.change_mode(play_mode)
     pass
 
 
 def draw():
+    global boat_x, boat_y
     clear_canvas()
     sea.image.clip_draw(0, 0, 1980, 1080, monitor_width / 2, monitor_height / 2, monitor_width, monitor_height)
+
+    boat.image.clip_draw(0, 0, 400, 400, boat_x, boat_y, 200, 200)
     update_canvas()
+    print(boat_y)
     pass
 
 def pause():
@@ -51,4 +70,7 @@ def pause():
 
 
 def resume():
+    pass
+
+def Animation():
     pass
